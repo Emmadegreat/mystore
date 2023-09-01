04d1117b100b60f05ecdd73b2c33cc91cf6a3b0a
Emma F. Mkpurunchi <mkpurunchiemma@gmail.com>
2023-09-01 17:23:20 +0100

    add

--- src/components/pages/contact.jsx
+++ src/components/pages/contact.jsx
@@ -1,31 +1,45 @@
-import GoogleMapReact from 'google-map-react'
-import React from 'react'
+import React, { useRef } from 'react'
+
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
-    const AnyReactComponent = ({ text }) => <div>{text}</div>;
-    const defaultProps = {
-    center: {
-      lat: 10.99835602,
-      lng: 77.01502627
-    },
-    zoom: 11
-  };
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm(
+            'service_hpwftru',
+            'template_0ko02r8',
+            form.current, 'uqeNFiEeopkQJod9d'
+        )
+        .then((result) => {
+            console.log(result.text);
+            console.log('error');
+        }, (error) => {
+            console.log(error.text);
+            console.log('success');
+        });
+    };
 
     return (
         <section className='contact-page'>
-            <div className="map" style={{width:"100%", height:"500px"}}>
-                <GoogleMapReact
-                    bootstrapURLKeys={{ key: "" }}
-                    defaultCenter={defaultProps.center}
-                    defaultZoom={defaultProps.zoom}
-                >
-                    <AnyReactComponent
-                        lat={59.955413}
-                        lng={30.337844}
-                        text="My Marker"
-                    />
 
-                </GoogleMapReact>
+            <div style={{ width: "100%" }}>
+                <iframe width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=3-6%20Alhaji%20Adejumo%20Avenue,%20Ilupeju%20Industrial%20Estate%20Ilupeju,%20Lagos.+(e-store)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.maps.ie/population/">Find Population on Map</a></iframe>
+            </div>
+
+            <div className="contact-form">
+                <form ref={form} onSubmit={sendEmail}>
+                    <label>Name</label>
+                    <input type="text" name="user_name" />
+                    <label>Email</label>
+                    <input type="email" name="user_email" />
+                    <label>Message</label>
+                    <textarea name="message" />
+                    <input type="submit" value="Send" />
+                </form>
             </div>
         </section>
     )
