7a150cb84064dc8d0bafccfe1dfd809f7b071aa1
Emma F. Mkpurunchi <mkpurunchiemma@gmail.com>
2023-09-07 18:22:45 +0100

    add

--- src/components/pages/contact.jsx
+++ src/components/pages/contact.jsx
@@ -17,34 +17,30 @@ const Contact = () => {
     const sendEmail = (e) => {
         e.preventDefault();
 
-        const sendDetails = {
-            user_name,
-            user_email,
-            message
+        const templateParams = {
+            fromName: user_name,
+            fromEmail: user_email,
+            message: message
         }
-
-        const Log =()=>console.log(user_email+user_name+message);
-
-
 
         emailjs.sendForm(
             'service_hpwftru',//service-id
             'template_0ko02r8',//template id
             form.current,
             'uqeNFiEeopkQJod9d'//public-key
-            //sendDetails,
         )
-        .then((result) => {
-            console.log(result.text);
-            console.log('error');
-        }, (error) => {
-            if (!error) {
-               redirect(<Link to="/" />)
-            } else {
-                console.log('there is error');
-            }
-            //console.log(error.text);
-            //console.log('success');
+        .then((response) => {
+            if (response) {
+               alert(`Hello ${user_name}, your mail has been submitted successfully`)
+             console.log('success', response);
+           }
+            setUser_name('');
+            setUser_email('');
+            setMessage('')
+        })
+            .catch((error) => {
+            alert("error")
+            console.log(error, 'error');
         });
     };
 
@@ -52,31 +48,34 @@ const Contact = () => {
         <section className='contact-page'>
 
             <div style={{ width: "100%" }}>
-                <iframe width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=3-6%20Alhaji%20Adejumo%20Avenue,%20Ilupeju%20Industrial%20Estate%20Ilupeju,%20Lagos.+(e-store)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.maps.ie/population/">Find Population on Map</a></iframe>
+                <iframe style={{ width:"100%", height:"400",frameborder:"0",scrolling:"no",marginHeight:"0",marginWidth:"0"}} src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=3-6%20Alhaji%20Adejumo%20Avenue,%20Ilupeju%20Industrial%20Estate%20Ilupeju,%20Lagos.+(e-store)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.maps.ie/population/">Find Population on Map</a></iframe>
             </div>
 
             <div className="contact-form">
                 <h1>Send us a mail</h1><br />
                 <form ref={form} onSubmit={sendEmail}>
-                    <label for="name">Name</label>
+                    <label htmlFor="name">Name</label>
                     <input
                         type="text"
-                        name="user_name"
+                        name='user_name'
                         required
-
+                        value={user_name}
+                        onChange={(e)=>setUser_name(e.target.value)}
                     />
-                    <label for="email">Email</label>
+                    <label htmlFor="email">Email</label>
                     <input
                         type="email"
                         name="user_email"
                         required
-
+                        value={user_email}
+                        onChange={(e)=>setUser_email(e.target.value)}
                     />
-                    <label for="message">Message</label>
+                    <label htmlFor="message">Message</label>
                     <textarea
-                        name="message"
+                        name='message'
                         required
-
+                        value={message}
+                        onChange={(e)=>setMessage(e.target.value)}
                     />
                     <button type="submit">Send</button>
                 </form>
